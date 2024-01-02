a4ff2322a3cb3c46fcaa3167aaf04504277f830c
ste7anste7an <stefan@vmi1504892.contaboserver.net>
2024-01-02 09:08:03 +0100

    added patches

--- extensions/ioty_pupremote.py
+++ extensions/ioty_pupremote.py
@@ -1,10 +1,10 @@
-from ioty.pupremote import  PUPRemoteSensor, SPIKE_ULTRASONIC
+from pupremote import  PUPRemoteSensor, SPIKE_ULTRASONIC
 
 _pupsensor = None
 
 def init(sensor_id=SPIKE_ULTRASONIC):
     global _pupsensor
-    _pupsensor = PUPRemoteSensor(sensor_id=SPIKE_ULTRASONIC)
+    _pupsensor = PUPRemoteSensor(sensor_id=SPIKE_ULTRASONIC,power=True)
 
 def add_command(pup_function,to_hub_fmt="",from_hub_fmt=""):
     _pupsensor.add_command(pup_function,to_hub_fmt,from_hub_fmt)
--- patch/patch.py
+++ patch/patch.py
@@ -1,15 +1,38 @@
 import os
 import shutil
+import re
 
-r=open("patch.txt").read()
+r=open("patch.txt","rb").read()
 
 
-dst_dir="/home/stefan/projects/github_ioty/ioty_pup/"
-src_dir="/home/stefan/projects/github_ioty/ioty_own/"
+dst_dir=b"/home/stefan/projects/ioty_stefan/public/"
+src_dir=b"/home/stefan/projects/ioty_own/"
+
+
+def re_insert_after(content,ins,reg):
+    match=re.search(reg,content,re.DOTALL)
+    #print(content)
+    print("===============")
+    print(reg)
+    print("================")
+    p=match.span()[1] # end position
+    print("content",match.span()[0],p)
+    return content[:p]+ins+content[p:]
 
 def insert_after(content,ins,search):
     p=content.find(search)
     p+=len(search)
+    return content[:p]+ins+content[p:]
+
+def re_insert_before(content,ins,reg):
+    print("re_insert_before")
+    #print(content)
+    print("===============")
+    print(reg)
+    print("================")
+    match=re.search(reg,content,re.DOTALL)
+    p=match.span()[0] # start position
+    print("content",p,match.span()[1])
     return content[:p]+ins+content[p:]
 
 def insert_before(content,ins,search):
@@ -18,16 +41,20 @@ def insert_before(content,ins,search):
 
 
 
-patches=r.split('####')
+patches=r.split(b'\n####\n')
+pp=0
 for patch in patches:
-    rule=patch.split('###\n')
-    cmd=rule[0].split('##\n')
-    print(cmd)
+    rule=patch.split(b'\n###\n')
+    cmd=rule[0].split(b'\n##\n')
+    print(cmd,len(cmd))
     if len(cmd)==1:
-        if cmd[0].strip()=="copy_files":
+        print(cmd[0])
+        if cmd[0].strip()==b"copy_files":
+            print("--> copy files")
             args=rule[1:]
             for arg in args:
-                f1,f2=arg.split('##\n')
+                #print(arg)
+                f1,f2=arg.split(b'\n##\n')
                 f1=f1.strip()
                 f2=f2.strip()
                 sf=src_dir+f1
@@ -37,12 +64,22 @@ for patch in patches:
                 os.makedirs(os.path.dirname(df), exist_ok=True)
                 shutil.copy(sf, df)
     elif len(cmd)==2:
+        pp+=1
         filename=dst_dir+cmd[0].strip()
         command=cmd[1].strip()
         args=rule[1:]
-        content=open(filename).read()
+        content=open(filename,'rb').read()
         print("red file",len(content))
         for arg in args:
-           s1,s2=arg.split('##\n')
-           print(filename,command,s1[:30],">>>>>>>>>>",s2[:30])
-
+            s1,s2=arg.split(b'\n##\n')
+            print(filename,command)
+            if command==b'insert_after':
+                content=insert_after(content,s1,s2)
+            elif command==b're_insert_after':
+                 content=re_insert_after(content,s1,s2)
+            elif command==b'insert_before':
+                content=insert_before(content,s1,s2)
+            elif command==b're_insert_before':
+                 content=re_insert_before(content,s1,s2)
+        #open(filename+b"_jnk",'wb').write(content)
+        open(filename,'wb').write(content)
